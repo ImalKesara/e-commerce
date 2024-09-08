442d64e624aec470f1802990271eea21af245ef1
LoneWolf <imalkesara2001@gmail.com>
2024-09-08 18:14:43 +0530

    added card shadn-svelte

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,11 +1,32 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import Navbar from '$lib/components/Navbar.svelte';
+	import { cn } from '$lib/utils';
+	let pathname = $derived($page.url.pathname);
+	let { children } = $props();
+	type NavLink = {
+		href: string;
+		text: string;
+	};
 </script>
 
 <Navbar>
-	{@render navLink({ href: '/admin', text: 'D' })}
+	{@render navLink({ href: '/admin', text: 'Dashboard' })}
+	{@render navLink({ href: '/admin/product', text: 'Product' })}
+	{@render navLink({ href: '/admin/users', text: 'Customers' })}
+	{@render navLink({ href: '/admin/orders', text: 'Orders' })}
 </Navbar>
 
-{#snippet navLink({ href, text }: { href: string; text: string })}
-	<a {href}>{text}</a>
+<div class="container my-6">
+	{@render children()}
+</div>
+
+{#snippet navLink({ href, text }: NavLink)}
+	<a
+		{href}
+		class={cn(
+			'p-4 capitalize hover:bg-secondary hover:text-secondary-foreground focus-visible:bg-secondary focus-visible:text-secondary',
+			pathname === href && 'bg-background text-foreground'
+		)}>{text}</a
+	>
 {/snippet}
---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card';
+
+	type DashboardCardProps = {
+		title: string;
+		subtitle: string;
+		body: string;
+	};
+</script>
+
+<div class="grid grid-cols-1 gap-x-3 md:grid-cols-2 lg:grid-cols-3">
+	{@render dashboardCard({ title: 'Total Sales', subtitle: 'This month', body: '$10,00,000' })}
+	{@render dashboardCard({ title: 'Total Sales', subtitle: 'This month', body: '$10,00,000' })}
+	{@render dashboardCard({ title: 'Total Sales', subtitle: 'This month', body: '$10,00,000' })}
+</div>
+
+{#snippet dashboardCard({ title, subtitle, body }: DashboardCardProps)}
+	<Card.Root>
+		<Card.Header>
+			<Card.Title>{title}</Card.Title>
+			<Card.Description>{subtitle}</Card.Description>
+		</Card.Header>
+		<Card.Content>
+			<p>{body}</p>
+		</Card.Content>
+	</Card.Root>
+{/snippet}

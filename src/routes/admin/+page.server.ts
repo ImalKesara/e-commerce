3bde3cfc09ce5db845c6eda3817329224a76f128
LoneWolf <imalkesara2001@gmail.com>
2024-09-14 21:48:39 +0530

    Done part 1

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -7,16 +7,45 @@ async function getSalesData() {
 	});
 	return {
 		amount: (data._sum.priceInCent || 0) / 100,
-        numberofSales : data._count
+		numberofSales: data._count
 	};
 }
 
 async function getUserData() {
-    const userCount = await db.user.count();
+	const [userCount, orderData] = await Promise.all([
+		await db.user.count(),
+		await db.order.aggregate({
+			_sum: { priceInCent: true }
+		})
+	]);
+
+	return {
+		userCount,
+		avgValuePerUser: userCount === 0 ? 0 : (orderData._sum.priceInCent || 0) / userCount / 1000
+	};
+}
+
+async function getProductData() {
+	const [activeCount, inactiveCount] = await Promise.all([
+		db.product.count({ where: { isAvailableForPurchase: true } }),
+		db.product.count({ where: { isAvailableForPurchase: false } })
+	]);
+
+	return {
+		activeCount,
+		inactiveCount
+	};
 }
 
 export const load = async () => {
+	const [salesData, userData, productData] = await Promise.all([
+		getSalesData(),
+		getUserData(),
+		getProductData()
+	]);
 	return {
-
-    };
+		salesData,
+		userData,
+		productData
+	};
 };
--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -2,18 +2,32 @@
 	import * as Card from '$lib/components/ui/card';
 	import { formatCurrency, formatNumber } from '$lib/utils';
 
-
 	type DashboardCardProps = {
 		title: string;
 		subtitle: string;
 		body: string;
 	};
+
+	let { data } = $props();
+	let { productData, userData, salesData } = $derived(data);
 </script>
 
 <div class="grid grid-cols-1 gap-x-3 md:grid-cols-2 lg:grid-cols-3">
-	{@render dashboardCard({ title: 'Total Sales', subtitle: `${formatNumber(2000)} Orders`, body: `${formatCurrency(500)}` })}
-	{@render dashboardCard({ title: 'Total Sales', subtitle: `${formatNumber(3000)} Orders`, body: `${formatCurrency(1500)}` })}
-	{@render dashboardCard({ title: 'Total Sales', subtitle: `${formatNumber(3500)} Orders`, body: `${formatCurrency(1200)}` })}
+	{@render dashboardCard({
+		title: 'Sales',
+		subtitle: `${formatNumber(salesData.numberofSales)} Orders`,
+		body: `${formatCurrency(salesData.amount)}`
+	})}
+	{@render dashboardCard({
+		title: 'Total Sales',
+		subtitle: `${formatNumber(3000)} Orders`,
+		body: `${formatCurrency(1500)}`
+	})}
+	{@render dashboardCard({
+		title: 'Total Sales',
+		subtitle: `${formatNumber(3500)} Orders`,
+		body: `${formatCurrency(1200)}`
+	})}
 </div>
 
 {#snippet dashboardCard({ title, subtitle, body }: DashboardCardProps)}

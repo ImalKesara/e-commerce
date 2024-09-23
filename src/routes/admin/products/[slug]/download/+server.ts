e7f0ad4c7ed772c9aa884f4562ceac22ea77c56d
LoneWolf <imalkesara2001@gmail.com>
2024-09-24 00:28:50 +0530

    svelkit api endpoint(for download)

---
+++ src/routes/admin/products/[slug]/download/+server.ts
@@ -0,0 +1,20 @@
+import { db } from '../../../../../hooks.server.js';
+import fs from 'fs/promises';
+
+export const GET = async ({ params: { slug } }) => {
+	const product = await db.product.findUnique({
+		where: { p_id: slug },
+		select: { filePath: true, p_name: true }
+	});
+	if (!product) return new Response('Prodct does not exist');
+	const { size } = await fs.stat(product.filePath);
+	const file = await fs.readFile(product.filePath);
+	//mp3 , jpg,png......
+	const extension = product.filePath.split('.').pop();
+
+	return new Response(file,{
+		headers:{
+			'Content-Type' : `application/${extension}`
+		}
+	});
+};

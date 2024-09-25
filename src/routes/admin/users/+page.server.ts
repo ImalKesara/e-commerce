de9b610abee5c998081bee5d65c3f3da5ad7cc7e
LoneWolf <imalkesara2001@gmail.com>
2024-09-25 19:19:58 +0530

    delete user

--- src/routes/admin/users/+page.server.ts
+++ src/routes/admin/users/+page.server.ts
@@ -1,3 +1,4 @@
+import { request } from 'http';
 import { db } from '../../../hooks.server';
 export const load = async () => {
 	return {
@@ -12,4 +13,13 @@ export const load = async () => {
 			}
 		})
 	};
+};
+export const actions = {
+	deleteUser: async ({ request }) => {
+		const formData = await request.formData();
+		const u_id = formData.get('u_id') as string;
+		await db.user.delete({
+			where: { u_id }
+		});
+	}
 };

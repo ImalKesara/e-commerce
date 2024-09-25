cccfa342959bd29ad6ea4ca1a65361e658f069b4
LoneWolf <imalkesara2001@gmail.com>
2024-09-25 19:20:53 +0530

    delete orders

--- src/routes/admin/orders/+page.server.ts
+++ src/routes/admin/orders/+page.server.ts
@@ -4,11 +4,24 @@ export const load = async () => {
 	return {
 		orders: await db.order.findMany({
 			select: {
-				o_id: true
+				o_id: true,
+				priceInCent: true,
+				product: { select: { p_name: true } },
+				user: { select: { email: true } }
 			},
 			orderBy: {
 				o_id: 'asc'
 			}
 		})
 	};
+};
+
+export const actions = {
+	deleteOrder: async ({ request }) => {
+		const formData = await request.formData();
+		const o_id = formData.get('o_id') as string;
+		await db.order.delete({
+			where:{o_id}
+		})
+	}
 };

fd183b1347040a2ee09e9f2228c0512cf562e996
LoneWolf <imalkesara2001@gmail.com>
2024-09-26 22:36:28 +0530

    set cookies / login

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,12 +1,10 @@
 import { loginFormSchema } from '$lib/formSchema';
 import { fail } from '@sveltejs/kit';
-import { request } from 'http';
 import { generateId } from 'lucia';
 import { Argon2id } from 'oslo/password';
 import { superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
-import { db } from '../../hooks.server.js';
-
+import { db, lucia } from '../../hooks.server.js';
 
 export const load = async () => {
 	return {
@@ -21,10 +19,21 @@ export const actions = {
 			return fail(400, { form });
 		}
 		const userId = generateId(15);
-		const hashedPassword  = await new Argon2id().hash(form.data.password);
+		const hashedPassword = await new Argon2id().hash(form.data.password);
 		await db.user.create({
-			
-		})
-
+			data: {
+				email: 'testing@gmail.com',
+				hashedPassword,
+				username: form.data.username,
+				isAdmin: true,
+				u_id: userId
+			}
+		});
+		const session = await lucia.createSession(userId, {});
+		const sessionCookie = lucia.createSessionCookie(session.id);
+		event.cookies.set(sessionCookie.name, sessionCookie.value, {
+			path: '.',
+			...sessionCookie.attributes
+		});
 	}
 };

3621cd3051c47472354c3118e8bdad5d4a696a27
LoneWolf <imalkesara2001@gmail.com>
2024-09-26 22:19:26 +0530

    oslo/password

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,9 +1,30 @@
-import { loginFormSchema } from "$lib/formSchema";
-import { superValidate } from "sveltekit-superforms";
-import { zod } from "sveltekit-superforms/adapters";
+import { loginFormSchema } from '$lib/formSchema';
+import { fail } from '@sveltejs/kit';
+import { request } from 'http';
+import { generateId } from 'lucia';
+import { Argon2id } from 'oslo/password';
+import { superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { db } from '../../hooks.server.js';
 
-export const load = (async () => {
+
+export const load = async () => {
 	return {
 		form: await superValidate(zod(loginFormSchema))
 	};
-});
+};
+
+export const actions = {
+	default: async (event) => {
+		const form = await superValidate(event, zod(loginFormSchema));
+		if (!form.valid) {
+			return fail(400, { form });
+		}
+		const userId = generateId(15);
+		const hashedPassword  = await new Argon2id().hash(form.data.password);
+		await db.user.create({
+			
+		})
+
+	}
+};
